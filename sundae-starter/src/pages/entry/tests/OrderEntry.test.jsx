8fcec962624232208416eb9d76ca8187af6d9539
Diamanto Skiada <diamanto9203@gmail.com>
2024-11-17 18:56:19 +0200

    alert banner test

---
+++ sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,32 @@
+import { logRoles, render, screen } from "@testing-library/react";
+import OrderEntry from "../OrderEntry";
+import { http, HttpResponse } from 'msw';
+import { server } from "../../../mocks/server";
+
+// put test.only(''..) in order to run only this one test
+// and skip the others
+// or test.skip to skip this test
+test('handle errors for scoops and toppings', async () => {
+    server.resetHandlers(
+        http.get(
+            'http://localhost:3030/scoops', () => {
+                return new HttpResponse(null, {status: 500})
+            }
+        ),
+        http.get(
+            'http://localhost:3030/toppings', () => {
+                return new HttpResponse(null, {status: 500})
+            }
+        )
+    )
+    const { container } = render(<OrderEntry />);
+
+    // const alerts = await screen.findAllByRole('alert', { name: /An unexpected error occur. Please try again later./i});
+    // const alerts = await screen.findAllByRole('alert'); this passes
+
+    const alerts = await screen.findAllByText("An unexpected error occurred. Please try again later.");
+
+    logRoles(container);
+
+    expect(alerts).toHaveLength(2);
+})
