18973e81f47474779ebaf9abe2b8884997573dc9
Diamanto Skiada <diamanto9203@gmail.com>
2025-01-20 16:54:54 +0200

    context adjustments

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,8 +1,14 @@
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
+import OrderEntry from "./pages/entry/OrderEntry";
+import Container from "react-bootstrap/Container"
+
 function App() {
   return (
-    <div>
-      <h1>Sundaes on Demand</h1>
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        <OrderEntry/>
+      </OrderDetailsProvider>
+    </Container>
   );
 }
 
