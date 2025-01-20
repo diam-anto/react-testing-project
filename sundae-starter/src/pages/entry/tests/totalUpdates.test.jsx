eb8cbcbc08e0632a207cec51ee75dd1d4f241d35
Diamanto Skiada <diamanto9203@gmail.com>
2025-01-20 17:16:12 +0200

    test for toppings

--- sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
@@ -27,3 +27,26 @@ test("update scoop subtotal when scoops change", async () => {
   await user.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update topping subtotal when scoops change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />, {wrapper: OrderDetailsProvider});
+
+  // make sure total starts out $0.00
+  const toppingsTotal = screen.getByText("Toppings total: $", { exact: false });
+  expect(toppingsTotal).toHaveTextContent("0.00");
+
+  // update cherries and check the subtotal
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherriesCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+
+  // update hot fudge and check subtotal
+  const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot fudge",
+  });
+  await user.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+});
